3540f37806e84fc0a9df457238c39970725519d2
Jack Li <114454416+JackLiTX@users.noreply.github.com>
2022-09-26 21:32:35 -0500

    Update streamlit_app.py
    add build your own fruit section that read csv from fruit macro txt file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit
+import pandas as pd
 
 streamlit.title("My Parent's new healthy dinner")
 streamlit.header("Breakfast Menu")
@@ -7,3 +8,8 @@ streamlit.text("😊Ome a three and Blueberry Oatmeal")
 streamlit.text("👂Kale, Spinacch & Rocket Smoothie")
 streamlit.text("Hard-Boiled Free-Range Egg")
 
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
