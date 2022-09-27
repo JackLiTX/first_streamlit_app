4e3ba9bba90e05bddb46a034f9854fab95b0c370
Jack Li <114454416+JackLiTX@users.noreply.github.com>
2022-09-26 21:27:22 -0500

    Update streamlit_app.py
    add emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 
 streamlit.title("My Parent's new healthy dinner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega three and Blueberry Oatmeal")
-streamlit.text("Kale, Spinacch & Rocket Smoothie")
+streamlit.text("😊Ome a three and Blueberry Oatmeal")
+streamlit.text("👂Kale, Spinacch & Rocket Smoothie")
 streamlit.text("Hard-Boiled Free-Range Egg")
 
