91c26e24245b2ba7ce917d2c8d96491eb874da0a
Jack Li <114454416+JackLiTX@users.noreply.github.com>
2022-09-29 17:28:25 -0500

    Update streamlit_app.py
    add json data after data frame.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,7 @@ fruit_to_show = my_fruit_list.loc[fruit_selected]
 #display the table on the page
 streamlit.dataframe(fruit_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
