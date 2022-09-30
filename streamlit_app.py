a1189c2338fcf442c5528b72454555b05a9f09ac
Jack Li <114454416+JackLiTX@users.noreply.github.com>
2022-09-30 14:03:57 -0500

    Update streamlit_app.py
    fix the not bug in add_my_fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,6 @@ streamlit.header("The fruit load list containts:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
-if(not add_my_fruit):
+if(add_my_fruit):
   streamlit.write('Thanks for adding ', add_my_fruit)
 
